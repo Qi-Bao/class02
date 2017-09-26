a0a4406e1cfed83f95b7f09d53551595271e4b59
Qi-Bao <31685821+Qi-Bao@users.noreply.github.com>
2017-09-26 15:54:01 +0800

    更新项目主要代码
    主要简单地实现了对字符数，单词数以及行数的统计。

--- class02.cpp
+++ class02.cpp
@@ -1,5 +1,48 @@
 #include "stdafx.h"
-int _tmain(int argc, _TCHAR* argv[])
-{ 
-  return 0;
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <string>
+using namespace std;
+
+void readTxt(string file);
+
+int main()
+{
+	cout << "welcome to Qibao's Class02." << endl;
+	readTxt("momo.txt");
+    return 0;
+}
+
+void readTxt(string file)
+{
+	int a=0, b=1, d=0;
+	ifstream infile;
+	infile.open(file.data());   //将文件流对象与文件连接起来 
+	assert(infile.is_open());   //若失败,则输出错误消息,并终止程序运行 
+	char c;
+	infile >> noskipws;
+	while (!infile.eof())
+	{
+		infile >> c;
+		if (c == ' ' || c == '\t' || c == '\n')
+		{
+			a++;
+		}
+		if (c == '\n')
+		{
+			b++;
+		}
+		if (c != ' ' && c != '\t' && c != '\n')
+		{
+			d++;
+		}
+		cout << c ;
+	}
+	cout << endl;
+	cout << "line:" << b << endl;
+	cout << "word:" << a << endl;
+	cout << "character:" << d << endl;
+	cin >> d;
+	infile.close();  //关闭文件输入流 
 }
